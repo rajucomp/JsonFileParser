7c53a2f559373630c0d1376f8e67eff18a276eff
Raju Gupta <cs135017@mnnit.ac.in>
2025-03-16 15:50:52 +0000

    Update JsonFileParser.java

--- JsonFileParser.java
+++ JsonFileParser.java
@@ -1,14 +1,12 @@
 package org.example;
 
-import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * @author raju.gupta
@@ -25,45 +23,23 @@ public final class JsonFileParser {
 
     public static void parse(final String filePath)
         throws IOException {
-        try (InputStream fis = new FileInputStream(filePath);
-            JsonReader reader = Json.createReader(fis)) {
-
-            JsonObject jsonObject = reader.readObject();
-            JsonArray actions = jsonObject.getJsonArray(ACTIONS);
-
-
-            for (JsonObject action : actions.getValuesAs(JsonObject.class)) {
-                JsonArray returnValue = action.getJsonArray(RETURN_VALUE);
-                for (JsonObject returnValueObject : returnValue.getValuesAs(JsonObject.class)) {
-                    System.out.println("Developer Name: " + returnValueObject.getString("developerName"));
-                    // Add more fields as needed
-                }
-            }
-        } catch (IOException e) {
-            System.out.println("An error occurred while reading the file: " + e.getMessage());
-            e.printStackTrace();
-            throw e;
-        }
+       final String content = Files.readString(Paths.get(filePath));
+        parseApiResponse(content);
     }
 
     public static void parseApiResponse(final String apiResponse) {
 
-        try(JsonReader reader = Json.createReader(new ByteArrayInputStream(apiResponse.getBytes()))) {
-            JsonObject jsonObject = reader.readObject();
-            JsonArray actions = jsonObject.getJsonArray(ACTIONS);
+        final JSONObject jsonObject = new JSONObject(apiResponse);
+        final JSONArray actions = jsonObject.getJSONArray(ACTIONS);
 
-            for (JsonObject action : actions.getValuesAs(JsonObject.class)) {
-                JsonArray returnValue = action.getJsonArray(RETURN_VALUE);
-                for (JsonObject returnValueObject : returnValue.getValuesAs(JsonObject.class)) {
-                    System.out.println("Developer Name: " + returnValueObject.getString("developerName"));
-                    // Add more fields as needed
-                }
+        for (int i = 0; i < actions.length(); i++) {
+            JSONObject action = actions.getJSONObject(i);
+            JSONArray returnValue = action.getJSONArray(RETURN_VALUE);
+            for (int j = 0; j < returnValue.length(); j++) {
+                JSONObject returnValueObject = returnValue.getJSONObject(j);
+                System.out.println("Developer Name: " + returnValueObject.getString("developerName"));
+                // Add more fields as needed
             }
-        }
-        catch (final Exception ex) {
-            System.out.println("An error occurred while parsing the API response: " + ex.getMessage());
-            ex.printStackTrace();
-            throw ex;
         }
     }
 }
